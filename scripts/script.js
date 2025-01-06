f797d51d334fbd7ec6e0642d9c0b8e72ddb2f418
aanchal691 <aanchal89500@gmail.com>
2025-01-06 17:45:55 +0530

    Add script file and hardcoded each item

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+let itemsContainerElement = document.querySelector('.items-container');
+let item = {
+    item_image: 'images/7.jpg',
+    rating: {
+        stars: 4.5,
+        noOfReviews: 1400,
+    },
+    company_name:'Nike',
+    item_name:'Nike Casual Shoes Wh',
+    current_price: 606,
+    original_price: 1045,
+    discount_percentage: 42,
+
+}
+itemsContainerElement.innerHTML = `
+<div class="item-container">
+    <img class="item-image" src="${item.item_image}" alt="item pic">
+    <div class="rating">
+        ${item.rating.stars} ⭐ | ${item.rating.noOfReviews}k
+    </div>
+    <div class="company-name">
+        ${item.company_name}
+    </div>
+    <div class="item-name">${item.item_name}</div>
+    <div class="price">
+        <span class="current-price">Rs ${item.current_price}</span>
+        <span class="original-price">${item.original_price}</span>
+        <span class="discount">(${item.discount_percentage}% OFF)</span>
+    </div>
+    <button class="btn-add-bag">Add To Cart</button>
+</div>`;
