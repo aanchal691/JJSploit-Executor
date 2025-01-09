a6ab95949232d4a2a35fc0084d5513640c81d7b8
aanchal691 <aanchal89500@gmail.com>
2025-01-09 17:53:43 +0530

    Update  bagitem

--- scripts/bag.js
+++ scripts/bag.js
@@ -11,36 +11,40 @@ function loadBagItemObjects(){
     bagItemsObject = bagItems.map(itemId=>{
         for(let i=0; i<items.length; i++){
             if(itemId == items[i].id){
-                return item[i];
+                return items[i];
             }
         }
     })
 }
 function displayBagItems(){
 let containerElement = document.querySelector('.bag-items-container');
-// containerElement.innerHTML = 
+let innerHTML = '';
+bagItemsObject.forEach(bagItem => {
+    innerHTML += generateItemHTML(bagItem);
+});
+containerElement.innerHTML = innerHTML;
 }
-function generateItemHTML(item){
+function generateItemHTML(items){
     return `
 
     <div class="bag-item-container">
         <div class="item-left-part">
-        <img class="bag-item-img" src="../${item.image}">
+        <img class="bag-item-img" src="../${items.image}">
         </div>
         <div class="item-right-part">
-        <div class="company">${item.company}</div>
-        <div class="item-name">${item.item_name}</div>
+        <div class="company">${items.company}</div>
+        <div class="item-name">${items.item_name}</div>
         <div class="price-container">
-            <span class="current-price">Rs ${item.current_price}</span>
-            <span class="original-price">Rs ${item.original_price}</span>
-            <span class="discount-percentage">(${item.discount_percentage}% OFF)</span>
+            <span class="current-price">Rs ${items.current_price}</span>
+            <span class="original-price">Rs ${items.original_price}</span>
+            <span class="discount-percentage">(${items.discount_percentage}% OFF)</span>
         </div>
         <div class="return-period">
-            <span class="return-period-days">${item.return_period} days</span> return available
+            <span class="return-period-days">${items.return_period} days</span> return available
         </div>
         <div class="delivery-details">
             Delivery by
-            <span class="delivery-details-days">${item.delivery_date}</span>
+            <span class="delivery-details-days">${items.delivery_date}</span>
         </div>
         </div>
 
